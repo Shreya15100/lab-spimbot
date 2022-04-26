f3d6d4147fe22efe889c6d41c937543cd9dec96c
Shreya15100 <shreya15@illinois.edu>
2022-04-25 21:27:56 -0500

    add includes

---
+++ spimbot.h
@@ -0,0 +1,21 @@
+#pragma once 
+
+#include "string"
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+
+using std::string;
+
+class Spimbot {
+
+    
+
+    public:
+
+
+
+    private:
+};
+
